c284ade20a9f94155b2e8ac70ba39795c810675a
Aldo <aldohassielacosta@gmail.com>
2025-03-15 03:10:40 -0700

    Limita el acceso al sistema añadiendo un tiempo de espera cada vez de tres intentos fallidos

--- CapaPresentacion/Login.Designer.cs
+++ CapaPresentacion/Login.Designer.cs
@@ -38,6 +38,7 @@
             this.btnIngresar = new FontAwesome.Sharp.IconButton();
             this.btnCancelar = new FontAwesome.Sharp.IconButton();
             this.label5 = new System.Windows.Forms.Label();
+            this.lblTiempoDeEspera = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -46,8 +47,9 @@
             this.label1.BackColor = System.Drawing.Color.Indigo;
             this.label1.Dock = System.Windows.Forms.DockStyle.Left;
             this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(301, 278);
+            this.label1.Size = new System.Drawing.Size(226, 226);
             this.label1.TabIndex = 0;
             // 
             // label2
@@ -56,9 +58,10 @@
             this.label2.BackColor = System.Drawing.Color.Indigo;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(12, 191);
+            this.label2.Location = new System.Drawing.Point(9, 155);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(256, 29);
+            this.label2.Size = new System.Drawing.Size(207, 25);
             this.label2.TabIndex = 1;
             this.label2.Text = "SISTEMA DE VENTA";
             // 
@@ -68,31 +71,31 @@
             this.iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.Store;
             this.iconPictureBox1.IconColor = System.Drawing.Color.White;
             this.iconPictureBox1.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.iconPictureBox1.IconSize = 164;
-            this.iconPictureBox1.Location = new System.Drawing.Point(52, 31);
-            this.iconPictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.iconPictureBox1.IconSize = 133;
+            this.iconPictureBox1.Location = new System.Drawing.Point(39, 25);
+            this.iconPictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.iconPictureBox1.Name = "iconPictureBox1";
-            this.iconPictureBox1.Size = new System.Drawing.Size(181, 164);
+            this.iconPictureBox1.Size = new System.Drawing.Size(136, 133);
             this.iconPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.iconPictureBox1.TabIndex = 2;
             this.iconPictureBox1.TabStop = false;
             // 
             // txtdocumento
             // 
-            this.txtdocumento.Location = new System.Drawing.Point(355, 91);
-            this.txtdocumento.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtdocumento.Location = new System.Drawing.Point(266, 74);
+            this.txtdocumento.Margin = new System.Windows.Forms.Padding(2);
             this.txtdocumento.Name = "txtdocumento";
-            this.txtdocumento.Size = new System.Drawing.Size(265, 22);
+            this.txtdocumento.Size = new System.Drawing.Size(200, 20);
             this.txtdocumento.TabIndex = 3;
             this.txtdocumento.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtdocumento_KeyDown);
             // 
             // txtclave
             // 
-            this.txtclave.Location = new System.Drawing.Point(355, 164);
-            this.txtclave.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtclave.Location = new System.Drawing.Point(266, 133);
+            this.txtclave.Margin = new System.Windows.Forms.Padding(2);
             this.txtclave.Name = "txtclave";
             this.txtclave.PasswordChar = '*';
-            this.txtclave.Size = new System.Drawing.Size(265, 22);
+            this.txtclave.Size = new System.Drawing.Size(200, 20);
             this.txtclave.TabIndex = 4;
             this.txtclave.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtdocumento_KeyDown);
             // 
@@ -100,18 +103,20 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(353, 64);
+            this.label3.Location = new System.Drawing.Point(265, 52);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(125, 16);
+            this.label3.Size = new System.Drawing.Size(100, 13);
             this.label3.TabIndex = 5;
             this.label3.Text = "Número documento";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(353, 137);
+            this.label4.Location = new System.Drawing.Point(265, 111);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(76, 16);
+            this.label4.Size = new System.Drawing.Size(61, 13);
             this.label4.TabIndex = 6;
             this.label4.Text = "Contraseña";
             // 
@@ -125,10 +130,10 @@
             this.btnIngresar.IconColor = System.Drawing.Color.Black;
             this.btnIngresar.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.btnIngresar.IconSize = 21;
-            this.btnIngresar.Location = new System.Drawing.Point(355, 222);
-            this.btnIngresar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnIngresar.Location = new System.Drawing.Point(266, 180);
+            this.btnIngresar.Margin = new System.Windows.Forms.Padding(2);
             this.btnIngresar.Name = "btnIngresar";
-            this.btnIngresar.Size = new System.Drawing.Size(123, 30);
+            this.btnIngresar.Size = new System.Drawing.Size(92, 24);
             this.btnIngresar.TabIndex = 7;
             this.btnIngresar.Text = "Ingresar";
             this.btnIngresar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -146,10 +151,10 @@
             this.btnCancelar.IconColor = System.Drawing.Color.Black;
             this.btnCancelar.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.btnCancelar.IconSize = 21;
-            this.btnCancelar.Location = new System.Drawing.Point(502, 222);
-            this.btnCancelar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnCancelar.Location = new System.Drawing.Point(376, 180);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(2);
             this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(119, 30);
+            this.btnCancelar.Size = new System.Drawing.Size(89, 24);
             this.btnCancelar.TabIndex = 8;
             this.btnCancelar.Text = "Cancelar";
             this.btnCancelar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -161,18 +166,29 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(380, 19);
+            this.label5.Location = new System.Drawing.Point(285, 15);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(217, 29);
+            this.label5.Size = new System.Drawing.Size(178, 25);
             this.label5.TabIndex = 9;
             this.label5.Text = "INICIAR SESION";
             // 
+            // lblTiempoDeEspera
+            // 
+            this.lblTiempoDeEspera.AutoSize = true;
+            this.lblTiempoDeEspera.Location = new System.Drawing.Point(265, 155);
+            this.lblTiempoDeEspera.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTiempoDeEspera.Name = "lblTiempoDeEspera";
+            this.lblTiempoDeEspera.Size = new System.Drawing.Size(0, 13);
+            this.lblTiempoDeEspera.TabIndex = 10;
+            // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(685, 278);
+            this.ClientSize = new System.Drawing.Size(514, 226);
+            this.Controls.Add(this.lblTiempoDeEspera);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.btnCancelar);
             this.Controls.Add(this.btnIngresar);
@@ -184,7 +200,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
@@ -206,5 +222,6 @@
         private FontAwesome.Sharp.IconButton btnIngresar;
         private FontAwesome.Sharp.IconButton btnCancelar;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblTiempoDeEspera;
     }
 }
--- CapaPresentacion/Login.cs
+++ CapaPresentacion/Login.cs
@@ -7,8 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
-
 using CapaEntidad;
 using CapaNegocio;
 
@@ -16,9 +14,24 @@ namespace CapaPresentacion
 {
     public partial class Login : Form
     {
+        private static int intentosFallidos;
+        private static DateTime FechaDesbloqueo;
+        private static Timer temporizador;
+        private static TimeSpan tiempoDeEspera;
         public Login()
         {
             InitializeComponent();
+            intentosFallidos = Properties.Settings.Default.IntentosFallidos;
+            FechaDesbloqueo = (intentosFallidos == 0) ? DateTime.Now : Properties.Settings.Default.FechaDesbloqueo;
+            if (FechaDesbloqueo > DateTime.Now) btnIngresar.Enabled = false;
+            double tiempo = Properties.Settings.Default.TiempoEspera;
+            if (tiempo > 0)
+            {
+                temporizador = new Timer();
+                temporizador.Interval = (FechaDesbloqueo - DateTime.Now).Seconds;
+                temporizador.Tick += ActualizarEtiquetaTiempo;
+                temporizador.Start();
+            }
         }
 
         private void btnCancelar_Click(object sender, EventArgs e)
@@ -28,28 +41,80 @@ namespace CapaPresentacion
 
         private void btnIngresar_Click(object sender, EventArgs e)
         {
-            //List<Usuario> TEST = new CN_Usuario().Listar();
-            Usuario ousuario = new CN_Usuario().Listar().Where(u => u.Documento == txtdocumento.Text && u.Clave == txtclave.Text).FirstOrDefault();
-            
-            if(ousuario == null)
+            Usuario ousuario = new CN_Usuario().Listar().Where(u => u.Documento == txtdocumento.Text).FirstOrDefault();
+
+            if (ousuario == null)
             {
-                MessageBox.Show("No se encontro el usuario", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                MessageBox.Show("No se encontró el usuario", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                 return;
             }
-            if(ousuario.Estado == false)
+            if (ousuario.Estado == false)
             {
                 MessageBox.Show("El usuario está desactivado", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                 return;
             }
+            if (ousuario.Clave != txtclave.Text)
+            {
+                intentosFallidos++;
+                
+                if (intentosFallidos % 3 == 2)
+                {
+                    MessageBox.Show($"Clave incorrecta\nIntento número: {intentosFallidos}\nAl siguiente intento fallido se bloqueará temporalmente el acceso.", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                else if (intentosFallidos % 3 == 0)
+                {
+                    MessageBox.Show($"Clave incorrecta\nEspere un tiempo", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    EsperarUnTiempito();
+                }
+                else
+                {
+                    MessageBox.Show($"Clave incorrecta\nIntentalo de nuevo\nIntento número: {intentosFallidos}", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
 
+                return;
+            }
+
+            intentosFallidos = 0;
+            Properties.Settings.Default.IntentosFallidos = intentosFallidos;
+            Properties.Settings.Default.Save();
             INICIO form = new INICIO(ousuario);
-
             form.Show();
             this.Hide();
-
             form.FormClosing += frm_closing;
         }
+        private void EsperarUnTiempito()
+        {
+            btnIngresar.Enabled = false;
+            int tiempoDeAumento = Convert.ToInt32(Math.Pow(((intentosFallidos / 3) * 30), 2)) / 60;
+            FechaDesbloqueo = DateTime.Now.AddSeconds(tiempoDeAumento);
+            temporizador = new Timer();
+            temporizador.Interval = tiempoDeAumento;
+            temporizador.Tick += ActualizarEtiquetaTiempo;
 
+            Properties.Settings.Default.FechaDesbloqueo = FechaDesbloqueo;
+            Properties.Settings.Default.IntentosFallidos = intentosFallidos;
+            Properties.Settings.Default.Save();
+
+            temporizador.Start();
+        }
+        private void ActualizarEtiquetaTiempo(Object sender, EventArgs e)
+        {
+            tiempoDeEspera = FechaDesbloqueo - DateTime.Now;
+            Properties.Settings.Default.TiempoEspera = tiempoDeEspera.TotalSeconds;
+            Properties.Settings.Default.Save();
+            lblTiempoDeEspera.Text = $"Tiempo de espera: {tiempoDeEspera.Hours}:{tiempoDeEspera.Minutes}:{tiempoDeEspera.Seconds}";
+
+            if (tiempoDeEspera.TotalSeconds <= 0)
+            {
+                temporizador.Stop();
+                btnIngresar.Enabled = true;
+                lblTiempoDeEspera.Text = "";
+
+                FechaDesbloqueo = DateTime.Now;
+                Properties.Settings.Default.FechaDesbloqueo = FechaDesbloqueo;
+                Properties.Settings.Default.Save();
+            }
+        }
         private void frm_closing(object sender, FormClosingEventArgs e)
         {
             txtdocumento.Text = "";
